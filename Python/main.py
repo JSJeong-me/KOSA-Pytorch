ba6531646232e9b8dd8c0e38639ca04598ae44d0
Joon Soo Jeong / Ph.D <54794815+JSJeong-me@users.noreply.github.com>
2021-05-21 11:49:53 +0900

    Python

---
+++ Python/main.py
@@ -0,0 +1,13 @@
+#https://docs.python.org/ko/3/library/__main__.html
+
+#main.py
+
+#from module import *
+import module
+
+
+if __name__ == "__main__":
+    print(__name__)
+
+    #hello()
+    module.hello()
---
+++ Python/module.py
@@ -0,0 +1,7 @@
+#module.py
+
+def hello():
+    print("Hello!")
+
+#if __name__=="__main__":
+#    print(__name__)
